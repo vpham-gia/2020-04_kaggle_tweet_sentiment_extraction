d0ec83991183726d400f7a2e737cff7b3f1dabe9
Vinh Pham-Gia <vinhpg45@gmail.com>
2020-04-18 12:34:55 +0200

    feat: set up settings for project and for VSCode

---
+++ tweet_sentiment_extraction/settings/base.py
@@ -0,0 +1,44 @@
+"""
+Contains all configurations for the projectself.
+Should NOT contain any secrets.
+
+>>> import settings
+>>> settings.DATA_DIR
+"""
+import os
+import logging
+
+# By default the data is stored in this repository's "data/" folder.
+# You can change it in your own settings file.
+REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+DATA_DIR = os.path.join(REPO_DIR, 'data')
+OUTPUTS_DIR = os.path.join(REPO_DIR, 'outputs')
+LOGS_DIR = os.path.join(REPO_DIR, 'logs')
+
+TESTS_DIR = os.path.join(REPO_DIR, 'tests')
+TESTS_DATA_DIR = os.path.join(TESTS_DIR, 'fixtures')
+
+
+# Logging
+def enable_logging(log_filename, logging_level=logging.DEBUG):
+    """Set loggings parameters.
+
+    Parameters
+    ----------
+    log_filename: str
+    logging_level: logging.level
+
+    """
+    with open(os.path.join(LOGS_DIR, log_filename), 'a') as file:
+        file.write('\n')
+        file.write('\n')
+
+    LOGGING_FORMAT = '[%(asctime)s][%(levelname)s][%(module)s] - %(message)s'
+    LOGGING_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    logging.basicConfig(
+        format=LOGGING_FORMAT,
+        datefmt=LOGGING_DATE_FORMAT,
+        level=logging_level,
+        filename=os.path.join(LOGS_DIR, log_filename)
+    )
